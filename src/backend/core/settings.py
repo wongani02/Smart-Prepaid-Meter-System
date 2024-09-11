30e4e3a153d0f26c1ab3508e17fa61a080ac43cb
wongani02 <81077944+wongani02@users.noreply.github.com>
2024-09-11 10:58:54 +0200

    Updates settings.py file

--- src/backend/core/settings.py
+++ src/backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic', 
 
     #local apps
     'accounts',
@@ -52,16 +53,19 @@ INSTALLED_APPS = [
     'import_export',
     'django_extensions',
     'channels',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -84,6 +88,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+CORS_ORIGIN_WHITELIST = (
+  'https://*.railway.app',
+)
 
 CHANNEL_LAYERS = {
     'default': {
@@ -145,6 +154,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
+STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
